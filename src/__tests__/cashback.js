7314341c9a47a5f2775d678c954689f70e22fa64
Mashkin E.V <mev.86@list.ru>
2023-10-22 14:41:19 +0300

    clarification added

--- src/__tests__/cashback.js
+++ src/__tests__/cashback.js
@@ -20,12 +20,13 @@ test('cashback at sum: 500', () => {
 //  expect(result).toBe(expected);
 // });
 
-// сокращенная pfgbcm^
+// сокращенная запись:
 test.each([
   ['gold', 100000, 5000],
   ['silver', 10000, 200],
   ['regular', 1000, 10],
   ['no', 500, 0],
+  // В Jest вы можете использовать %i для подстановки целочисленных значений, %s для подстановки строковых значений и %f для подстановки значений с плавающей запятой из входных данных
 ])('testing cashback function with %s status and %i amount', (_, amount, expected) => { // неиспользуемый параметр переименовываем в "_" или "$"
   const result = calculateCashback(amount);
   expect(result).toBe(expected);
